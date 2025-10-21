da93ce6b61b3ba60496fa629d0ef93291392d136
Sumant Kumar Singh <ssumantkumar50@gmail.com>
2025-10-21 12:15:05 +0530

    Updating File For Diployment

--- api/config/server.js
+++ api/config/server.js
@@ -1,9 +1,22 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('RENDER_EXTERNAL_URL') || env('URL', 'http://localhost:1337'),
+  proxy: env.bool('IS_PROXIED', true),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
