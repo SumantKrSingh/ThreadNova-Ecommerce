38c415ac8bc8999595301c021b5f7ee3cf5e0c1f
Sumant Kumar Singh <ssumantkumar50@gmail.com>
2025-10-21 20:57:27 +0530

    Adding host

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ['.onrender.com', 'threadnova-ecommerce.onrender.com']
+    host: true,
+    allowedHosts: ['.onrender.com', 'threadnova-ecommerce.onrender.com', 'localhost']
   },
   plugins: [react()],
 })
