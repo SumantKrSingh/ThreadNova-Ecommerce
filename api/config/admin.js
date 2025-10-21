9cf023189f41120c842b0895c106c8e56133811c
Sumant Kumar Singh <ssumantkumar50@gmail.com>
2025-10-22 00:40:46 +0530

    Fix session middleware for secure cookie issue

--- api/config/admin.js
+++ api/config/admin.js
@@ -24,6 +24,9 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    options: {
+      expiresIn: '7d',
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
@@ -33,6 +36,14 @@ module.exports = ({ env }) => ({
       salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
+  flags: {
+    nps: false,
+    promoteEE: false,
+  },
   url: '/admin',
   autoOpen: false,
+  // Force non-secure cookies
+  rateLimit: {
+    enabled: true,
+  },
 });
--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -14,20 +14,28 @@
 // For diployment
 
 module.exports = [
-  'strapi::errors',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: ['https://threadnova-ecommerce.onrender.com', 'http://localhost:1337'],
-    },
-  },
-  'strapi::security',
-  'strapi::poweredBy',
   'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      key: 'strapi.sid',
+      maxAge: 86400000,
+      autoCommit: true,
+      overwrite: true,
+      httpOnly: true,
+      signed: true,
+      rolling: false,
+      renew: false,
+      secure: false, // ← CRITICAL FIX
+      sameSite: 'lax',
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
