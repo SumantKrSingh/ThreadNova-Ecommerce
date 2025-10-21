7e3cb72658e50cc3c7cdfad5fb591c1bc1e2eb7d
Sumant Kumar Singh <ssumantkumar50@gmail.com>
2025-10-22 00:14:31 +0530

    Fix admin cookie security for Render deployment

--- api/config/admin.js
+++ api/config/admin.js
@@ -1,6 +1,35 @@
+// module.exports = ({ env }) => ({
+//   auth: {
+//     secret: env('ADMIN_JWT_SECRET'),
+//   },
+//   apiToken: {
+//     salt: env('API_TOKEN_SALT'),
+//   },
+//   transfer: {
+//     token: {
+//       salt: env('TRANSFER_TOKEN_SALT'),
+//     },
+//   },
+//   secrets: {
+//     encryptionKey: env('ENCRYPTION_KEY'),
+//   },
+//   flags: {
+//     nps: env.bool('FLAG_NPS', true),
+//     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+//   },
+// });
+
+
+// for diployment
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      maxRefreshTokenLifespan: 30 * 24 * 60 * 60 * 1000, // 30 days
+      maxSessionLifespan: 7 * 24 * 60 * 60 * 1000, // 7 days
+      // FIX: Set secure cookies only in production
+      cookieSecure: env('NODE_ENV') === 'production',
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
@@ -10,11 +39,6 @@ module.exports = ({ env }) => ({
       salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
+  url: '/admin',
+  autoOpen: false,
 });
