c93c2b8546a1fd4d252c2cf00c6e19ece24daca7
Sumant Kumar Singh <ssumantkumar50@gmail.com>
2025-10-21 13:00:01 +0530

    Updating server.js file

--- api/config/server.js
+++ api/config/server.js
@@ -12,12 +12,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://threadnova-ecommerce.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('RENDER_EXTERNAL_URL') || env('URL', 'http://localhost:1337'),
   proxy: env.bool('IS_PROXIED', true),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  allowedHosts: ['threadnova-ecommerce.onrender.com'],
 });
