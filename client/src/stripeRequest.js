18dbfeafe54c4d2d3423e3583c82e23b1e2d3fcc
Sumant Kumar Singh <ssumantkumar50@gmail.com>
2025-10-25 19:01:25 +0530

    Fix stripe URL

--- client/src/stripeRequest.js
+++ client/src/stripeRequest.js
@@ -1,6 +1,16 @@
+// for localhoast
+// import axios from "axios";
 
+// export const stripeRequest = axios.create({
+//     baseURL: "http://localhost:1337/api", // no /api here
+// });
+
+// for vercek diployment
 import axios from "axios";
 
 export const stripeRequest = axios.create({
-    baseURL: "http://localhost:1337/api", // no /api here
+    baseURL: import.meta.env.VITE_APP_API_URL,
+    headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_APP_API_TOKEN}`,
+    },
 });
